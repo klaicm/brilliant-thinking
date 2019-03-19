8da2b7643c4739c7f9aac4a9cd26415a1e69bd0c
klaicm <martin.klaic@gmail.com>
2019-03-19 19:57:58 +0100

    Initial components and templates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { TableComponent } from './table/table.component';
 
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'table', component: TableComponent },
+  { path: 'player', component: PlayerComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    TableComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
