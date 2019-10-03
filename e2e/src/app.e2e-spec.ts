ff4b44cf4a46f3f9a51d3ec15543236ea9dcdb0e
Martin Klaić <d313803@zaba.hr>
2019-10-03 16:01:39 +0200

    Styling of components

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to finance-me-app!');
+    expect(page.getParagraphText()).toEqual('Welcome to tennis-scores-app!');
   });
 });
