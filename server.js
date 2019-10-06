7ca804f3bc4c33243080c31a8cf5c789b571c05a
klaicm <martin.klaic@gmail.com>
2019-10-06 21:54:08 +0200

    Added Procfile. Changed directory path name.

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tennis-scores'));
+app.use(express.static(__dirname + '/dist/tennis-scores-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/tennis-scores/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tennis-scores-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
